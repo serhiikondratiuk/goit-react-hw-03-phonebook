1a85e7647f8d11a095f0dc40920252abf52211ea
Serhii Kondratiuk <sergey932015@gmail.com>
2022-01-11 06:01:19 +0200

    prop-types fix

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import PropTypes from "prop-types";
 import s from "./ContactForm.module.css";
 
 class Form extends Component {
@@ -58,5 +59,9 @@ class Form extends Component {
     );
   }
 }
+
+Form.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
 
 export default Form;
